c6a68244aff76dc6decdaa0b38a7bcf23de65f39
Jeff-cell-afk <78931120+Jeff-cell-afk@users.noreply.github.com>
2026-08-14 17:57:24 +0200

    Add batch deck building functions to init

--- simulateur_v5/simulateur/__init__.py
+++ simulateur_v5/simulateur/__init__.py
@@ -1,24 +1,28 @@
 from .models import Card, Pools, categories, colors
 from .pools import expand_pool, build_pools, validate_pools
-from .deck import build_main_deck_and_stock, build_trigger_deck_and_stock
+from .deck import (
+    build_main_deck_and_stock, build_trigger_deck_and_stock,
+    batch_build_main_decks, batch_build_trigger_decks,
+)
 from .grammar import (
-    BoundContext, Burn, Condition, Mill, Occurrence, OnCancel, Shuffle,
+    BoundContext, Burn, Condition, Mill, Occurrence, OnCancel, Scry, Shuffle,
     ShuffleAll, StockShuffle, StockSwap, TopCheck, check_balanced,
     max_possible_damage, max_possible_stock_gain, parse_condition,
     parse_occurrence, parse_occurrences, tokenize, validate_occurrence_costs,
 )
-from .game_system import GameSystem, run_single_trial, SystemBrokenError
+from .game_system import GameSystem, run_single_trial, run_trial_from_decks, SystemBrokenError
 from .simulation import generate_table, SimulationConfig
 
 __all__ = [
     "Card", "Pools", "categories", "colors",
     "expand_pool", "build_pools", "validate_pools",
     "build_main_deck_and_stock", "build_trigger_deck_and_stock",
+    "batch_build_main_decks", "batch_build_trigger_decks",
     "BoundContext", "Burn", "Condition", "Mill", "Occurrence", "OnCancel",
-    "Shuffle", "ShuffleAll", "StockShuffle", "StockSwap", "TopCheck",
+    "Scry", "Shuffle", "ShuffleAll", "StockShuffle", "StockSwap", "TopCheck",
     "check_balanced", "max_possible_damage", "max_possible_stock_gain",
     "parse_condition", "parse_occurrence", "parse_occurrences", "tokenize",
     "validate_occurrence_costs",
-    "GameSystem", "run_single_trial", "SystemBrokenError",
+    "GameSystem", "run_single_trial", "run_trial_from_decks", "SystemBrokenError",
     "generate_table", "SimulationConfig",
     ]
